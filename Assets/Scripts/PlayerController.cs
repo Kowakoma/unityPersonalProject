cefda309445d62adb5b19d4195ed3dee01223dc6
Kowakoma <wilmoord@yandex.ru>
2025-08-05 19:04:19 +0300

    add jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody _playerRb;
     [SerializeField] private float _speed;
     [SerializeField] private float _drag;
-    [SerializeField] private float _horizontalInput;
-    [SerializeField] private float _verticalInput;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _groundCheckDistance = 0.2f;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private Rigidbody _playerRb;
+    private bool _isOnGround;
+    private bool _jumpRequested;
+    private float _horizontalInput;
+    private float _verticalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,12 +25,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Input of movement on plane
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
+
+        // Input of jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumpRequested = true;
+        }
     }
+
     void FixedUpdate()
     {
+        // Physics of movement on plane
         Vector3 force = new Vector3(_horizontalInput, 0, _verticalInput) * _speed;
         _playerRb.AddForce(force, ForceMode.Force);
+
+        // IsOnGround check
+        _isOnGround = Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance, _groundLayer);
+        
+        // Physics of jump
+        if (_jumpRequested && _isOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _jumpRequested = false;
+            _isOnGround = false;
+        }
     }
 }
