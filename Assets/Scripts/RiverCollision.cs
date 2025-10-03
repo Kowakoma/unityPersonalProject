a2a66a6810513a7ab73b3cd40fd55b7ed1aa38bf
Kowakoma <wilmoord@yandex.ru>
2025-10-03 18:51:50 +0300

    perf: optimize RiverCollision with Rigidbody caching

--- Assets/Scripts/RiverCollision.cs
+++ Assets/Scripts/RiverCollision.cs
@@ -1,22 +1,25 @@
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class RiverCollision : MonoBehaviour
 {
-    private ScrollingWaterTexture _scrollingWaterTextureScript;
+    [Header("Flow force settings")]
     [SerializeField] private float _logFlowMultiplayer = 300f;
     [SerializeField] private float _branchFlowMultiplayer = 150f;
     [SerializeField] private float _playerFlowMultiplayer = 100f;
+    private ScrollingWaterTexture _scrollingWaterTextureScript;
+    private Dictionary<Collider, Rigidbody> _rigidbodyCache = new(); // Dictionary for obstacles rigidbody
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _scrollingWaterTextureScript = FindAnyObjectByType<ScrollingWaterTexture>();
-
     }
 
     void OnCollisionStay(Collision collision)
     {
-        Rigidbody Rb = collision.gameObject.GetComponent<Rigidbody>();
+        Rigidbody rb = GetCachedRigidbody(collision.collider);
 
         // Local var!!!
         float flowMultiplayer;
@@ -34,6 +37,20 @@ public class RiverCollision : MonoBehaviour
             flowMultiplayer = _playerFlowMultiplayer;
         }
 
-        Rb.AddForce(_scrollingWaterTextureScript.flowSpeed * flowMultiplayer, ForceMode.Force);
+        rb.AddForce(_scrollingWaterTextureScript.flowSpeed * flowMultiplayer, ForceMode.Force);
+    }
+
+    private Rigidbody GetCachedRigidbody(Collider collider)
+    {
+        // check is required value in cache?
+        if (!_rigidbodyCache.TryGetValue(collider, out Rigidbody rb))
+        {
+            // get not cached rigidbody from collider
+            rb = collider.GetComponent<Rigidbody>();
+            
+            // add rigidbody to cache
+            _rigidbodyCache[collider] = rb;
+        }
+        return rb;
     }
 }
