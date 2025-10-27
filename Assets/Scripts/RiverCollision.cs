85d30b4b4e4bb34e08cf53d9c50954a7cc5ff529
Kowakoma <wilmoord@yandex.ru>
2025-10-27 17:41:50 +0300

    chore: remove unused files and code

--- Assets/Scripts/RiverCollision.cs
+++ Assets/Scripts/RiverCollision.cs
@@ -5,7 +5,6 @@ public class RiverCollision : MonoBehaviour
     [Header("Flow force settings")]
     [SerializeField] private float _logFlowMultiplayer = 300f;
     [SerializeField] private float _branchFlowMultiplayer = 150f;
-    [SerializeField] private float _playerFlowMultiplayer = 1000f;
     private ScrollingWaterTexture _scrollingWaterTextureScript;
 
     void Start()
@@ -27,10 +26,6 @@ public class RiverCollision : MonoBehaviour
         else if (collision.gameObject.CompareTag("Branch"))
         {
             flowMultiplayer = _branchFlowMultiplayer;
-        }
-        else if (collision.gameObject.CompareTag("Player"))
-        {
-            flowMultiplayer = _playerFlowMultiplayer;
         }
 
         rb.AddForce(_scrollingWaterTextureScript.flowSpeed * flowMultiplayer, ForceMode.Force);
